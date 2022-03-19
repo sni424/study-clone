94bfd8e56d53e046460787eca924ce8880f3c50a
joshep Jo <jojonghyeok424@gmail.com>
2022-03-19 19:24:00 +0900

    배경 이미지

--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,14 +1,28 @@
 import styled from "styled-components";
 import Header from "../../components/Header";
 import SearchBar from "../../components/Search";
+import mainbg from '../../asset/Image/main-bg.png'
 
-const Container = styled.div``;
+const Container = styled.div`
+width: 100%;
+height: 100%;
+`;
+const BackImg = styled.div`
+background-image: url(${mainbg});
+max-width: 1980px;
+height: 50vw;
+background-size: cover;
+background-repeat: no-repeat;
+background-position: center;
+`;
 
 const Main = () => {
   return (
-    <Container>
+    <Container >
+      <BackImg>
       <Header />
       <SearchBar />
+      </BackImg>
     </Container>
   )
 }
